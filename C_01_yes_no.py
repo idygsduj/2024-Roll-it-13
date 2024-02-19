3e9e9bd2833905f69a1c015492ad6c640b61a6ef
idygsduj <128433594+idygsduj@users.noreply.github.com>
2024-02-19 13:12:18 +1300

    Miss showing how to drag / drop

--- C_01_yes_no.py
+++ C_01_yes_no.py
@@ -6,9 +6,10 @@ print("🎲🎲 Roll it 13 🎲🎲")
 while True:
     want_instructions = input("Do you want to read the instructions? ").lower()
 
-    if want_instructions == "yes":
+   # checks users enter yes (y) or no (n)
+    if want_instructions == "yes" or want_instructions == "y":
         print("you chose yes")
-    elif want_instructions == "no":
+    elif want_instructions == "no" or want_instructions == "n":
         print("you chose no")
     else:
         print("you did not choose a valid response")
